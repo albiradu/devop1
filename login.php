d869b051585bd8d2138ed7610ada0fe053d5b992
albertine <albiradu@gmail.com>
2025-02-25 22:58:49 +0200

    Login page is created.

--- login.php
+++ login.php
@@ -9,12 +9,12 @@
     <div class="container">
         <h2>Login</h2>
         <form action="login_action.php" method="POST">
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required><br><br>
-            
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required><br><br>
+
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required><br><br>
-            
+
             <input type="submit" value="Login">
         </form>
     </div>
